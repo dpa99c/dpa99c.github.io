ebd9e72b2ee56282afd7719db517ab07726c2358
Dave Alden <dpa99c@gmail.com>
2020-07-17 16:24:16 +0100

    wait for serviceworker to be ready before fetching

--- serviceworker-test/index.js
+++ serviceworker-test/index.js
@@ -1,12 +1,24 @@
 var targetUrl = "https://dpa99c.github.io/test/mailto.html";
 
+navigator.serviceWorker.addEventListener('message', event => {
+	log("service worker: "+event.data.msg);
+	if(event.data.msg === "activating"){
+		window.serviceWorkerActivated = true;
+		document.dispatchEvent(new Event("service-worker-activated"));
+	}
+});
+
 function onReady(){
 	log("Fetching target URL");
 
-	navigator.serviceWorker.addEventListener('message', event => {
-		log("service worker: "+event.data.msg);
-	});
+	if(serviceWorkersNotSupported || serviceWorkerActivated){
+		doFetch();
+	}else{
+		document.addEventListener("service-worker-activated", doFetch, false);
+	}
+}
 
+function doFetch(){
 	fetch(targetUrl)
 		.then(function(){
 			log("Fetch successful");
--- serviceworker-test/registerworker.js
+++ serviceworker-test/registerworker.js
@@ -2,7 +2,9 @@
 if('serviceWorker' in navigator) {
     navigator.serviceWorker.register('serviceworker.js').then(function (registration) {
         log('Service Worker registered');
+        window.serviceWorkerRegistered = true;
     });
 }else{
 	log("Service Workers are not available");
+    window.serviceWorkersNotSupported = true;
 }
